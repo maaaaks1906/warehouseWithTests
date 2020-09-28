05476508c79f2b3b9ed05eeabf75e60c4a1da496
maksymilian <maksymilian.kaczmarek@gmail.com>
2020-09-28 19:36:25 +0000

    Created constructor and method in ItemService and AddItemUseCase

--- src/main/java/com/maks/warehouse/warehouse/domain/service/ItemService.java
+++ src/main/java/com/maks/warehouse/warehouse/domain/service/ItemService.java
@@ -1,4 +1,11 @@
 package com.maks.warehouse.warehouse.domain.service;
 
+import com.maks.warehouse.warehouse.domain.model.Item;
+
+import java.util.List;
+
 public interface ItemService {
+
+    List<Item> getItems();
+
 }
--- src/main/java/com/maks/warehouse/warehouse/domain/usecase/AddItemUseCase.java
+++ src/main/java/com/maks/warehouse/warehouse/domain/usecase/AddItemUseCase.java
@@ -1,4 +1,18 @@
 package com.maks.warehouse.warehouse.domain.usecase;
 
+import com.maks.warehouse.warehouse.domain.model.Item;
+import com.maks.warehouse.warehouse.domain.service.ItemService;
+
+import java.util.List;
+
 public class AddItemUseCase {
+    private ItemService itemService;
+
+    public AddItemUseCase(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    public List<Item> invoke() {
+        return itemService.getItems();
+    }
 }
