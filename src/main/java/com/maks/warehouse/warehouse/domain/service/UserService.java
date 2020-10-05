caf0a8a5d889f1b2304b241806c5f281729297d4
maksymilian <maksymilian.kaczmarek@gmail.com>
2020-10-05 18:46:08 +0000

    FT-1 Added getUser by username method in UserService

--- src/main/java/com/maks/warehouse/warehouse/domain/service/UserService.java
+++ src/main/java/com/maks/warehouse/warehouse/domain/service/UserService.java
@@ -4,8 +4,11 @@ package com.maks.warehouse.warehouse.domain.service;
 import com.maks.warehouse.warehouse.domain.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
+
+    Optional<User> getUser(String username);
 
     List<User> getUsers();
 
